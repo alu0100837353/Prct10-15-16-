16762809900ed57686eb2d07a589a7472914b44e
alu0100845235 <alu0100845235@ull.edu.es>
2015-11-06 01:04:43 +0000

    Añadidas comprobaciones de tipos en el initialize

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -2,6 +2,18 @@ class Biblio
     attr_reader :autores, :titulo, :serie, :editorial, :n_edicion, :fecha_publicacion, :isbn
     
     def initialize(autores, titulo, serie, editorial, n_edicion, fecha_publicacion, isbn)
+        raise ArgumentError, "Autores debe ser un array" unless autores.is_a?(Array)
+        raise ArgumentError, "Autores debe tener al menos 1 valor" unless autores.size >= 1
+        raise ArgumentError, "El título debe se un string" unless titulo.is_a?(String)
+        if !serie.nil?
+            raise ArgumentError, "La serie debe se un string" unless serie.is_a?(String)
+        end
+        raise ArgumentError, "La editorial debe se un string" unless editorial.is_a?(String)
+        raise ArgumentError, "El número de edicion debe se un numero" unless n_edicion.is_a?(Numeric)
+        raise ArgumentError, "La fecha debe se un string" unless fecha_publicacion.is_a?(String)
+        raise ArgumentError, "La isbn debe se un array" unless isbn.is_a?(Array)
+        raise ArgumentError, "La isbn debe tener al menos 1 valor" unless isbn.size >= 1
+        
         @autores = autores
         @titulo = titulo
         @serie = serie
@@ -53,9 +65,11 @@ class Biblio
         
         # imprime la serie
         if(serie != nil)
-            puts "(" + serie + ")"
+            puts "(#{@serie})"
         end
+        
+        # imprime editorial, edicion y fecha
+        puts "#{@editorial};  #{@edicion} edition (#{@fecha_publicacion})"
+        
     end
 end
-
-
