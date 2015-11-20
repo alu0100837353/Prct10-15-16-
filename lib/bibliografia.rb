36fea060bedb18412d4a20aae85834ee61717daf
alu0100837353 <alu0100837353@ull.edu.es>
2015-11-20 11:16:51 +0000

    Creada estructura, partiendo de la práctica anterior

--- lib/bibliografia.rb
+++ lib/bibliografia.rb
@@ -4,4 +4,6 @@ module Bibliografia
   require 'bibliografia/biblio.rb'
   require 'bibliografia/node.rb'
   require 'bibliografia/linkedlist.rb'
+  require 'reference/libro.rb'
+  require 'reference/periodical.rb'
 end
